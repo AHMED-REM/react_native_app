0cccd34ba5c42836d512296e58de788e952dc654
ahmed-rem <remaida.ahmed@gmail.comm>
2025-12-18 18:20:50 +0100

    first commit

---
+++ ScreenLogin.js
@@ -0,0 +1,107 @@
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image, ImageBackground } from 'react-native';
+
+export default function ScreenLogin() {
+
+  return (
+    <ImageBackground
+      source={require('./assets/img_bg.jpg')}
+      style={styles.container}>
+
+      <View style={styles.loginpanel}>
+
+        <Image
+          source={require('./assets/img_user.jpg')}
+          style={styles.imgprofile} />
+
+        <TextInput
+          style={styles.txtinpt}
+          placeholder='Username' />
+
+        <TextInput
+          style={styles.txtinpt}
+          placeholder='Password'
+          secureTextEntry={true} />
+
+        <TouchableOpacity
+          style={styles.btn}>
+          <Text style={styles.txtbtn}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{ width: "70%", padding: 10, alignItems: "center", justifyContent: "center" }}>
+          <Text style={styles.txtlink}>Create a new Account</Text>
+        </TouchableOpacity>
+
+      </View>
+
+    </ImageBackground>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  loginpanel: {
+    width: '90%',
+    height: '70%',
+    backgroundColor: 'rgba(255, 255, 255, 0.7)',
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  imgprofile: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderBlockColor: 'black',
+    borderWidth: 1,
+    elevation: 2,
+    shadowColor: 'black',
+    marginBottom: 50
+  },
+  txtinpt: {
+    width: '70%',
+    backgroundColor: 'white',
+    padding: 12,
+    borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 1,
+    margin: 10
+  },
+  txtbtn: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+  txtlink: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: "bold",
+    textDecorationLine: "underline"
+  },
+  btn: {
+    borderRadius: 5,
+    backgroundColor: 'black',
+    width: "70%",
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50
+  },
+});
+
+
+
+
+
+
+
+
+
+
