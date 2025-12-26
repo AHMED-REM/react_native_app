b5e9d419dddc7940d595cd95c438a07b5d3b10f7
ahmed-rem <remaida.ahmed@gmail.comm>
2025-12-26 18:14:20 +0100

    commit 23

--- Header.js
+++ Header.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 
-export default function Header() {
+export default function Header(props) {
 
     return (
         <View style={styles.headercontainer}>
@@ -11,8 +11,8 @@ export default function Header() {
             />
 
             <View style={styles.infoscontainer}>
-                <Text style={styles.txtname}>ali</Text>
-                <Text style={styles.txtemail}>ali@gmail.com</Text>
+                <Text style={styles.txtname}>{props.name}</Text>
+                <Text style={styles.txtemail}>{props.email}</Text>
             </View>
 
             <TouchableOpacity onPress={() => { }}>
--- ScreenDashboard.js
+++ ScreenDashboard.js
@@ -1,12 +1,20 @@
 import { StyleSheet, View, Text } from 'react-native';
+import Header from './Header';
+import Card from './Card';
 
 export default function ScreenDashboard() {
 
   return (
     <View style={styles.container}>
+      <Header name="ali" email="ali@gmail.com" />
+      <Header name="mohamed" email="mohamed@gmail.com" />
+      <Header name="saiid" email="saiid@gmail.com" />
+      <Card />
     </View >
   );
+
 }
+
 
 const styles = StyleSheet.create({
   container: {
